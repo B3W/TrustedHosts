1cedc86cd028ba693ee8a7537c3166492f5c607e
Weston Berg <westieberg@gmail.com>
2018-08-06 19:08:55 -0500

    Spawning powershell sessions to allow retrieval from remote computers. Added credential as mandatory parameter for creating powershell sessions Added error handling and console logging with verbosity. (08/06/2018)

--- Get-TrustedHosts.ps1
+++ Get-TrustedHosts.ps1
@@ -5,11 +5,14 @@
 ### Author: Weston Berg
 ### Date: Aug. 5, 2018
 #####
+
 Function Get-TrustedHosts {
     [CmdletBinding()]
     param(
         [parameter(Mandatory=$true,ValueFromPipeline=$true)]
-        [string[]]$ComputerName
+        [string[]]$ComputerName,
+        [parameter(Mandatory=$true)]
+        [PSCredential]$Credential
     )
 
     # Pre-Processing
@@ -19,13 +22,100 @@ Function Get-TrustedHosts {
     # Get the Trusted Hosts from WSMan and 'return' them as output
     Process{
     
-        # Get the Trusted Hosts list from WSMan
-        $TrustedHosts = Get-Item WSMan:\$ComputerName\Client\TrustedHosts | Select -ExpandProperty Value
-    
-        # Format the output
-        $Output = "$ComputerName : $TrustedHosts"
+        # Test network connection to the computer 
+        Write-Verbose "Checking connection to $ComputerName..."
 
-        $Output
+        if(Test-Connection -ComputerName $ComputerName -Count 1 -Quiet) {
+
+             Write-Verbose "Connection to $ComputerName verified`n"
+
+            try {
+ 
+                # Open PowerShell Session
+                Write-Verbose "Creating PowerShell session..."
+
+                try {
+
+                    $PsSession = New-PSSession -ComputerName $ComputerName -Credential $Credential -ErrorAction Stop 
+
+                } catch {
+
+                    Write-Host "Error occurred during creation of PowerShell session" -ForegroundColor Red
+
+                    # Throw error to outer catch
+                    throw
+
+                }
+
+                Write-Verbose "PowerShell session created successfully!`n"
+
+ 
+                # Retrieve Trusted Hosts
+                Write-Verbose "Retrieving Trusted Hosts from $ComputerName..."
+
+                try {
+
+                    $RemoteHosts = Invoke-Command -Session $PsSession -ErrorAction Stop -ScriptBlock 
+                        { 
+                            # Get the Trusted Hosts list from WSMan
+                            $TrustedHosts = Get-Item WSMan:\localhost\Client\TrustedHosts | Select -ExpandProperty Value
+    
+                            # Format the output
+                            $Output = "$ComputerName : $TrustedHosts"
+
+                            $Output 
+                        }
+
+                    $RemoteHosts
+
+                } catch { 
+
+                    Write-Host "Error while retrieving Trusted Hosts from $ComputerName" -ForegroundColor Red
+
+                    # Remove PowerShell session before exiting
+                    Remove-PSSession $PsSession
+
+                    # Throw error to outer catch
+                    throw
+
+                }
+
+                Write-Verbose "Trusted Hosts retrieved successfully`n"
+
+
+                # Remove the PowerShell session
+                Write-Verbose "Removing PowerShell session...`n" 
+
+                try {
+
+                    Remove-PSSession $PsSession -ErrorAction Stop
+
+                } catch {
+
+                    Write-Host "Error while removing PowerShell session" -ForegroundColor Red
+
+                    # Throw error to outer catch
+                    throw
+
+                }
+
+                Write-Verbose "PowerShell session removed`n" 
+
+            } catch {
+
+                # Display error
+                Write-Host $Error[0] -ForegroundColor Red
+
+            }
+ 
+
+        } else {
+
+            Write-Verbose "Unable to connect to machine $ComputerName"
+
+            "$ComputerName : Unable To Connect"
+
+        }
 
     }
 
@@ -35,4 +125,4 @@ Function Get-TrustedHosts {
 }
 
 # Testing
-"localhost", "localhost" | Get-TrustedHosts
+"localhost", "localhost" | Get-TrustedHosts -Credential (Get-Credential) -Verbose
